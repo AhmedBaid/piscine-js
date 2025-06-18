c3007aba97989f5644a01c2df467ea9a3961f9fd
abaid <abaid@example.com>
2025-06-18 10:21:22 +0100

    declarations.js

---
+++ declarations.js
@@ -0,0 +1,4 @@
+const str = "`\\/\"'"
+const arr = [4, '2'];
+const obj = {str:"hello", num:500, bool:true,undef:undefined};
+const nested = {arr:[4,undefined,'2'], obj:{str:"hello", num:500, bool:false}};
