0d064e178832fb0e819014c2dedce2730b3e5b53
abaid <abaid@example.com>
2025-06-18 10:35:43 +0100

    declarations.js

--- declarations.js
+++ declarations.js
@@ -1,4 +1,10 @@
-const str = "`\\/\"'"
-const arr = [4, '2'];
-const obj = {str:"hello", num:500, bool:true,undef:undefined};
-const nested = {arr:[4,undefined,'2'], obj:{str:"hello", num:500, bool:false}};
+const str = "`\\/\"'";
+const arr = [4, "2"];
+Object.freeze(arr);
+const obj = { str: "hello", num: 500, bool: true, undef: undefined };
+Object.freeze(obj);
+const nested = {
+  arr: Object.freeze([4, undefined, "2"]),
+  obj: Object.freeze({ str: "hello", num: 500, bool: false }),
+};
+Object.freeze(nested);
