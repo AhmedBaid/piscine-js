1145a9ae54d365a572707ec5d18be5d067f86a2d
abaid <abaid@example.com>
2025-06-19 17:10:06 +0100

    block-chain.js

---
+++ block-chain.js
@@ -0,0 +1,14 @@
+const blockChain = (data, prev) => {
+    if (prev == undefined) {
+        prev = { index: 0, hash: '0' }
+    }
+    const block = {}
+    block.index = prev.index + 1
+    block.hash = hashCode(block.index + prev.hash + JSON.stringify(data))
+    block.data = data
+    block.prev = prev
+    block.chain = (nextData) => {
+        return blockChain(nextData, block)
+    }
+    return block
+}
