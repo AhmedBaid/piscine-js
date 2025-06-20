a46d7f07c34647dea66c95502a402b8fc7610d22
abaid <abaid@example.com>
2025-06-20 13:31:54 +0100

    elementary.js

---
+++ elementary.js
@@ -0,0 +1,50 @@
+function multiply(a, b) {
+    let res = 0
+    if (b < 0) {
+        a = -a
+        b = -b
+    }
+    for (let i = 0; i < b; i++) {
+        res += a
+    }
+
+    return res
+}
+
+function divide(a, b) {
+    let count = 0
+    let sign = 1
+    if (a > 0 && b < 0) {
+        sign = -1
+        b = -b
+    }
+    if (a < 0 && b > 0) {
+        sign = -1
+        a = -a
+    }
+    if (a < 0 && b < 0) {
+        a = -a
+        b = -b
+    }
+    while (a >= b) {
+        a -= b
+        count++
+    }
+    return multiply(count, sign)
+}
+
+
+function modulo(a, b) {
+    let sign = 1
+    if (a < 0 && b > 0) {
+        sign = -sign
+        a = -a
+    }
+    if (a < 0 && b < 0) {
+        sign = -sign
+        a = -a
+        b = -b
+    }
+    const mod = a - multiply(divide(a, b), b)
+    return mod * sign
+}
