f371bf253579667beb2d4ca2f7327b55057e1844
abaid <abaid@example.com>
2025-06-20 14:30:53 +0100

    index

---
+++ index-of.js
@@ -0,0 +1,9 @@
+const indexOf = (arr,value, optIndex) => {
+    if (optIndex==undefined) {
+        for (let i = 0; i < arr.length; i++) {
+            if(value==arr[i]){
+                return 
+            }
+        }
+    }
+};
