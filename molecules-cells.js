8ccc3143f72625031588efdeec7f9f86fd42b310
abaid <abaid@example.com>
2025-06-21 15:01:17 +0100

    molecules.js

---
+++ molecules-cells.js
@@ -0,0 +1,37 @@
+const RNA = (str) => {
+  let res = "";
+  let arr = [
+    ["G", "C"],
+    ["C", "G"],
+    ["T", "A"],
+    ["A", "U"],
+  ];
+  let mapp = new Map(arr);
+  for (let i = 0; i < str.length; i++) {
+    if (mapp.has(str[i])) {
+      res += mapp.get(str[i]);
+    } else {
+      res += str[i];
+    }
+  }
+  return res;
+};
+const DNA = (str) => {
+  let res = "";
+  let arr = [
+    ["C", "G"],
+    ["G", "C"],
+    ["A", "T"],
+    ["U", "A"],
+  ];
+  let mapp = new Map(arr);
+  for (let i = 0; i < str.length; i++) {
+    if (mapp.has(str[i])) {
+      res += mapp.get(str[i]);
+    } else {
+      res += str[i];
+    }
+  }
+  return res;
+};
+console.log(RNA("TAGC"));
