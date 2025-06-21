30af4f3bc1451ef504d6d8660f9cadc0c2ff2dd8
abaid <abaid@example.com>
2025-06-21 16:21:55 +0100

    reverser.js

--- reverser.js
+++ reverser.js
@@ -5,22 +5,3 @@ const reverse = (strORarr) => {
   }
   return typeof strORarr === "string" ? arr.join("") : arr;
 };
-const split = (str, sep) => {
-  let result = [];
-  let current = "";
-  let i = 0;
-
-  while (i < str.length) {
-    if (str.slice(i, i + sep.length) === sep) {
-      result.push(current);
-      current = "";
-      i += sep.length; // Skip the full separator
-    } else {
-      current += str[i];
-      i++;
-    }
-  }
-
-  result.push(current); // Push the last segment
-  return result;
-};
