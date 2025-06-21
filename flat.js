1ae86b2c8a08d865de238cabf5668bdb5eef9b17
abaid <abaid@example.com>
2025-06-21 19:06:35 +0100

    flat.js

--- flat.js
+++ flat.js
@@ -1,14 +1,12 @@
 const flat = (arr, depth = 1) => {
- 
   let arrr = [];
   for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])&& depth!= 0) {
-      const kk = flat(arr[i], depth - 1);
-      arrr.push(...kk);
+    if (Array.isArray(arr[i]) && depth > 0) {
+      const sigmant = flat(arr[i], depth - 1);
+      arrr.push(...sigmant);
     } else {
       arrr.push(arr[i]);
     }
   }
   return arrr;
 };
-console.log(flat([1]));
