b2ff8a6dedfda8e87fadf24d47b03caf3a36bf54
AhmedBaid <baidahmed2005@example.com>
2025-06-22 11:29:41 +0100

    find-expressions.js handle espace

--- find-expression.js
+++ find-expression.js
@@ -1,32 +1,34 @@
+const add4 = "+4";
+const mul2 = "*2";
 const findExpression = (nb) => {
-  let str = "1 ";
+  let str = "1";
   let val = 1;
   if (!iseven(nb)) {
     return undefined;
   }
   while (val < nb) {
     if (val + 4 == nb) {
-      str += add4 + " ";
+      str += " " + add4;
       return str;
     }
     let b = nb / val;
     if (!iseven(b)) {
       if (val + 4 == nb) {
         val = val + 4;
-        str += add4 + " ";
+        str += " " + add4;
         return str;
       } else if (val + 4 < nb) {
         val = val + 4;
-        str += add4;
+        str += " " + add4;
       }
     } else {
       if (val * 2 == nb) {
         val = val * 2;
-        str += mul2 + " ";
+        str += " " + mul2;
         return str;
       } else if (val * 2 < nb) {
         val = val * 2;
-        str += mul2 + " ";
+        str += " " + mul2;
       }
     }
   }
