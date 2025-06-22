348754bda3a8f26c4186a21c44f4024f4669c270
AhmedBaid <baidahmed2005@example.com>
2025-06-22 10:08:04 +0100

    regex ajmii

---
+++ find-expression.js
@@ -0,0 +1,12 @@
+const findExpression = (nb) => {
+  let str = "";
+  let target = 0
+  if (iseven(nb)) {
+  } else {
+    return undefined;
+  }
+};
+const iseven = (nb) => nb % 2 === 0;
+console.log(iseven(0));
+console.log(findExpression(8));
+
