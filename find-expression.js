e570c455a27b7e853b8dcb6d8a219795a154400a
AhmedBaid <baidahmed2005@example.com>
2025-06-22 11:27:20 +0100

    find-expressions.js

--- find-expression.js
+++ find-expression.js
@@ -1,12 +1,36 @@
 const findExpression = (nb) => {
-  let str = "";
-  let target = 0
-  if (iseven(nb)) {
-  } else {
+  let str = "1 ";
+  let val = 1;
+  if (!iseven(nb)) {
     return undefined;
   }
+  while (val < nb) {
+    if (val + 4 == nb) {
+      str += add4 + " ";
+      return str;
+    }
+    let b = nb / val;
+    if (!iseven(b)) {
+      if (val + 4 == nb) {
+        val = val + 4;
+        str += add4 + " ";
+        return str;
+      } else if (val + 4 < nb) {
+        val = val + 4;
+        str += add4;
+      }
+    } else {
+      if (val * 2 == nb) {
+        val = val * 2;
+        str += mul2 + " ";
+        return str;
+      } else if (val * 2 < nb) {
+        val = val * 2;
+        str += mul2 + " ";
+      }
+    }
+  }
+  return str;
 };
 const iseven = (nb) => nb % 2 === 0;
-console.log(iseven(0));
 console.log(findExpression(8));
-
