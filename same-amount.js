b31b3f025c1a68011a217b5a9c19450d924f9761
AhmedBaid <baidahmed2005@example.com>
2025-06-22 13:44:40 +0100

    ex4 in regex

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,7 @@
 const sameAmount = (str, regex1, regex2) => {
-  regex1 = new RegExp(regex1.source, "g");
-  regex2 = new RegExp(regex2.source, "g");
+  regex1 = new RegExp(regex1, "g");
+  regex2 = new RegExp(regex2, "g");
   let len1 = str.match(regex1) !== null ? str.match(regex1) : [];
   let len2 = str.match(regex2) !== null ? str.match(regex2) : [];
-  if (len1.length == len2.length) {
-    return true;
-  }
-  return false;
+  return len1.length == len2.length;
 };
