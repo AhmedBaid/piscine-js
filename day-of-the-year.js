445b89645b0d172e16749ade6231d852ea991d1f
AhmedBaid <you@example.com>
2025-06-26 19:09:53 +0100

    exercice dayofleapyear

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,6 @@
 const dayOfTheYear = (date) => {
   let d = new Date("0001-01-01");
-  d.setFullYear(date.getFullYear() - 1);
+  d.setUTCFullYear(date.getUTCFullYear());
   const diff = date - d;
   const oneDay = 1000 * 60 * 60 * 24;
   let res = Math.floor(diff / oneDay) + 1;
