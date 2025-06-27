7cf174d16f0176b1e12e0fd23ae330dba2a9050c
AhmedBaid <you@example.com>
2025-06-27 17:45:24 +0100

    mapper

--- mapper.js
+++ mapper.js
@@ -1,7 +1,19 @@
 const map = (arr, func) => {
-  let arr = [];
+  let arrr = [];
   for (let i = 0; i < arr.length; i++) {
-    arr.push(func(arr[i]));
+    arrr.push(func(arr[i], i, arr));
   }
-  return arr
+  return arrr;
+};
+
+const flatMap = (arr, func) => {
+  let arrr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(func(arr[i],i,arr))) {
+      arrr.push(...func(arr[i], i, arr));
+    } else {
+      arrr.push(func(arr[i], i, arr));
+    }
+  }
+  return arrr;
 };
