2920193a53956ebcc0defb14a3c349182f296642
AhmedBaid <you@example.com>
2025-06-27 16:46:14 +0100

    using reduce

---
+++ using-reduce.js
@@ -0,0 +1,24 @@
+const adder = (arr, s = 0) => {
+  let res = arr.reduce((a, b) => a + b, s);
+  return res;
+};
+
+const sumOrMul = (arr, s = 0) => {
+  let res = arr.reduce((a, b) => {
+    console.log(a, b);
+
+    if (b % 2 == 0) {
+      return a * b;
+    } else {
+      return a + b;
+    }
+  }, s);
+  return res;
+};
+
+const funcExec = (arrOFfunc,s) => {
+    let res = arrOFfunc.reduce((a,b)=>{
+        return a(b)
+    },s)
+    return res
+};
