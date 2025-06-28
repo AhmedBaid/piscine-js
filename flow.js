778a51f4df52ee5d33e644f5e3e40a5011ea8152
AhmedBaid <you@example.com>
2025-06-28 12:01:42 +0100

    flow

--- flow.js
+++ flow.js
@@ -2,7 +2,7 @@ const flow = (arrOFfunc) => {
   return function (...params) {
     let res = arrOFfunc[0](...params);
     for (let i = 1; i < arrOFfunc.length; i++) {
-      res += arrOFfunc[i](res);
+      res = arrOFfunc[i](res);
     }
     return res;
   };
