4c6cc1ff4493255fd1ee8389e00e93cc9758a8aa
AhmedBaid <you@example.com>
2025-06-28 18:27:45 +0100

    curiify

---
+++ currify.js
@@ -0,0 +1,10 @@
+const currify = (func) => {
+  let curried = (...args) => {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return (next) => curried(args, next);
+    }
+  };
+  return curried;
+};
