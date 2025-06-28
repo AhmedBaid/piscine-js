555a7c76a31160a023664a938cde9e001082d6f9
AhmedBaid <you@example.com>
2025-06-28 13:59:15 +0100

    dom

--- get-them-all.js
+++ get-them-all.js
@@ -8,23 +8,23 @@ export const getArchitects = () => {
 };
 export const getClassical = () => {
   let arr = [];
-  let architects = document.querySelectorAll("classical");
-  let Nonarchitects = document.querySelectorAll(":not(classical)");
+  let architects = document.querySelectorAll(".classical");
+  let Nonarchitects = document.querySelectorAll(":not(.classical)");
   arr.push(architects);
   arr.push(Nonarchitects);
   return arr;
 };
 export const getActive = () => {
   let arr = [];
-  let architects = document.querySelectorAll("classical.active");
-  let Nonarchitects = document.querySelectorAll(":not(classical.active)");
+  let architects = document.querySelectorAll(".classical.active");
+  let Nonarchitects = document.querySelectorAll(":not(.classical.active)");
   arr.push(architects, Nonarchitects);
   return arr;
 };
 export const getBonannoPisano = () => {
   let arr = [];
   let architects = document.getElementById("#BonannoPisano");
-  let Nonarchitects = document.querySelectorAll("classical.active");
+  let Nonarchitects = document.querySelectorAll(".classical.active");
   arr.push(architects, Nonarchitects);
   return arr;
 };
