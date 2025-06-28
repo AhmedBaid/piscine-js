8519eed9832113934e297e45b47d3d385762b2bb
AhmedBaid <you@example.com>
2025-06-28 16:19:57 +0100

    build break

---
+++ build-brick-and-break.js
@@ -0,0 +1,34 @@
+export const build = (nb) => {
+  const body = document.querySelector("body");
+  let index = 1;
+  const interval = setInterval(() => {
+    if (index <= nb) {
+      let newbrick = document.createElement("div");
+      newbrick.id = `brick-${index}`;
+      if ((index - 1) % 3 == 1) {
+        newbrick.dataset.foundation = "true";
+      }
+      body.appendChild(newbrick);
+      newbrick.innerHTML = index;
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 100);
+  return interval;
+};
+
+export const repair = (...ids) => {
+  ids.forEach((id) => {
+    let brick = document.getElementById(`${id}`);
+    if (brick.dataset.foundation == "true") {
+      brick.dataset.foundation == "in progress";
+    }
+  });
+};
+
+export const destroy = () => {
+  let lastdiv = document.querySelectorAll("div");
+  if (lastdiv.length == 0) return;
+  lastdiv[0].remove();
+};
