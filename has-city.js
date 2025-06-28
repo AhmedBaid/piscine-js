af8ad2f75e7a2456848d891b4f5a95de7bb6f1b8
AhmedBaid <you@example.com>
2025-06-28 11:15:36 +0100

    has city

--- has-city.js
+++ has-city.js
@@ -1,11 +1,9 @@
 const hasCity = (country, arr) => {
   return function (city) {
-    arr.array.forEach((element) => {
-      if (element == city) {
-        return city + " is a city from " + country;
-      } else {
-        return city + " is not a city from " + country;
-      }
-    });
+    if (arr.includes(city)) {
+      return city + " is a city from " + country;
+    } else {
+      return city + " is not a city from " + country;
+    }
   };
 };
