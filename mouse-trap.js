a4eff0b3dc8363f85c0a3a7ae53696379d4af1d0
AhmedBid <you@example.com>
2025-06-30 15:40:07 +0100

    mouse trap

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,4 +1,3 @@
-let current = null;
 export function createCircle() {
   document.addEventListener("click", (event) => {
     let circle = document.createElement("div");
@@ -7,47 +6,53 @@ export function createCircle() {
     circle.style.top = `${event.clientY - 25}px`;
     circle.style.left = `${event.clientX - 25}px`;
     document.body.appendChild(circle);
-    current = circle;
   });
 }
 
 export function moveCircle() {
   document.addEventListener("mousemove", (e) => {
-    let box = document.querySelector(".box");
-    if (!box || !current) return;
-    let X = e.clientX;
-    let Y = e.clientY;
-    current.style.top = `${Y - 25}px`;
-    current.style.left = `${X - 25}px`;
-
-    let posbox = box.getBoundingClientRect();
-    const inside =
-      posbox.left < e.clientX - 25 &&
-      posbox.right > e.clientX + 25 &&
-      posbox.top < e.clientY - 25 &&
-      posbox.bottom > e.clientY + 25;
-    if (inside) {
-      current.style.background = "var(--purple)";
-      box.appendChild(current);
-    }
-    if (current.style.background == "var(--purple)") {
-      if (e.clientX < posbox.left + 25) {
-        current.style.left = posbox.left + "px";
-      }
-      if (e.clientX > posbox.right - 25) {
-        current.style.left = posbox.right - 50 + "px";
-      }
-      if (e.clientY < posbox.top + 25) {
-        current.style.top = posbox.top + "px";
-      }
-      if (e.clientY > posbox.bottom - 25) {
-        current.style.top = posbox.bottom - 50 + "px";
-      }
+    let circles = document.querySelectorAll(".circle");
+    if (circles.length > 0) {
+      const last = circles[circles.length - 1];
+      let X = e.clientX;
+      let Y = e.clientY;
+      last.style.top = `${Y - 25}px`;
+      last.style.left = `${X - 25}px`;
     }
   });
 }
 export function setBox() {
   let box = document.createElement("div");
+
   box.className = "box";
-  document.body.appendChild(box);
+  document.body.append(box);
+  addEventListener("mousemove", (e) => {
+    let circle = document.querySelectorAll(".circle");
+    let posbox = box.getBoundingClientRect();
+    if (circle.length > 0) {
+      let last = circle[circle.length - 1];
+      if (
+        e.clientX > posbox.left + 25 &&
+        e.clientX < posbox.right - 25 &&
+        e.clientY > posbox.top + 25 &&
+        e.clientY < posbox.bottom - 25
+      ) {
+        last.style.background = "var(--purple)";
+      }
+      if (last.style.background == "var(--purple)") {
+        if (e.clientX < posbox.left + 25) {
+          last.style.left = posbox.left + "px";
+        }
+        if (e.clientX > posbox.right - 25) {
+          last.style.left = posbox.right - 50 + "px";
+        }
+        if (e.clientY < posbox.top + 25) {
+          last.style.top = posbox.top + "px";
+        }
+        if (e.clientY > posbox.bottom - 25) {
+          last.style.top = posbox.bottom - 50 + "px";
+        }
+      }
+    }
+  });
 }
