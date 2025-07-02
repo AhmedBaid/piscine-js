fc01f81e323b7cd4a4d0721ba31f4cbb7ddfe05f
abaid <you@example.com>
2025-07-02 18:19:36 +0100

    fusion

--- fusion.js
+++ fusion.js
@@ -1,23 +1,23 @@
 const fusion = (...objects) => {
   let res = objects[0];
-
-  for (let i = 1; i < objects.length; i++) {
-    for (let key in objects[i]) {
-      if (objects[i].hasOwnProperty(key)) {
-        if (Array.isArray(objects[i][key]) && Array.isArray(res[key])) {
-          res[key] = res[key].concat(objects[i][key]);
-        }else if(typeof res[key] == "string" && typeof objects[i][key] == "string"){
-          res[key] = res[key].concat(" "+objects[i][key]);
-        }else if(typeof res[key] == "number" && typeof objects[i][key] == "number"){
-          res[key] = res[key]+objects[i][key];
-        }else if(typeof res[key] == "object" && typeof objects[i][key] == "object"){
-          fusion(res[key],objects[i][key])
-        }else{
-          res[key]=objects[i][key]
+  objects.forEach((obj) => {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (Array.isArray(obj[key]) && Array.isArray(res[key])) {
+          res[key] = res[key].concat(obj[key]);
+        } else if (typeof res[key] == "string" && typeof obj[key] == "string") {
+          res[key] = res[key].concat(" " + obj[key]);
+        } else if (typeof res[key] == "number" && typeof obj[key] == "number") {
+          res[key] = res[key] + obj[key];
+        } else if (typeof res[key] == "object" && typeof obj[key] == "object") {
+          fusion(res[key], obj[key]);
+        } else {
+          res[key] = obj[key];
         }
       }
     }
-  }
+  });
   return res;
 };
+
 
