cc9ebd692c763e78a4c713b4ed5ff75bed174bbc
abaid <you@example.com>
2025-07-02 15:43:39 +0100

    pick and omit

--- pick-omit.js
+++ pick-omit.js
@@ -1,26 +1,26 @@
-const pick = (obj,strORarr)=>{
-    let res = {}
-    for (let key in obj){
-        if(obj.hasOwnProperty(key)){
-            if(strORarr.include(key)){
-                res[key]=obj[key]
-            }
-        }else{
-            continue
-        }
+const pick = (obj, strORarr) => {
+  let res = {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (strORarr.includes(key)) {
+        res[key] = obj[key];
+      }
+    } else {
+      continue;
     }
-    return res
-}
-const omit = (obj,strORarr)=>{
-    let res = {}
-    for (let key in obj){
-        if(obj.hasOwnProperty(key)){
-            if(!strORarr.include(key)){
-                res[key]=obj[key]
-            }
-        }else{
-            continue
-        }
+  }
+  return res;
+};
+const omit = (obj, strORarr) => {
+  let res = {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (!strORarr.includes(key)) {
+        res[key] = obj[key];
+      }
+    } else {
+      continue;
     }
-    return res
-}
+  }
+  return res;
+};
