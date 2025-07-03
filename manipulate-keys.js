52f1cf4a646c9c29b4b503f5fd3d927da9ae0662
abaid <you@example.com>
2025-07-03 14:20:14 +0100

    manipulate keys

---
+++ manipulate-keys.js
@@ -0,0 +1,30 @@
+const filterKeys = (obj, func) => {
+  let res = {};
+  for (let key in obj) {
+    if (func(key)) {
+      res[key] = obj[key];
+    }
+  }
+  return res;
+};
+
+const mapKeys = (obj, func) => {
+  let res = {};
+  for (const key in obj) {
+    let newkey = func(key);
+    res[newkey] = obj[key];
+  }
+  return res;
+};
+
+const reduceKeys = (obj, func, init) => {
+  let res = "";
+  if (init != undefined) {
+    res = init;
+  }
+  for (const key in obj) {
+    res = func(res, key);
+  }
+  return res;
+};
+
