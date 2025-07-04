b7d0f2bc5a4b6171fff1f210cc455a4c6871ef68
abaid <you@example.com>
2025-07-04 11:13:17 +0100

    deep copy

---
+++ deep-copy.js
@@ -0,0 +1,15 @@
+const deepCopy = (param) => {
+  let res = [];
+  if (Array.isArray(param)) {
+    param.forEach((p) => {
+      if (Array.isArray(p)) {
+        res.push(deepCopy(p));
+      } else {
+        res.push(p);
+      }
+    });
+  } else {
+    return {...param};
+  }
+  return res;
+};
