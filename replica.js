d8ad0da8b98c57db2742baca3a6515905428f0bb
abaid <you@example.com>
2025-07-04 12:52:36 +0100

    replica

--- replica.js
+++ replica.js
@@ -1,6 +1,13 @@
-const replica = (target,...objects)=>{
-    objects.forEach((obj)=>{
-return obj
-    })
-}
-
+const replica = (target, ...objects) => { 
+  objects.forEach((obj) => {
+    for (let key in obj) {
+     const value = obj[key]
+      if (typeof value === 'object' && value !== null && !Array.isArray(value)) {        
+        replica(target[key],value)
+      } else {
+        target[key]=value
+      }
+    }
+  });
+  return target;
+};
