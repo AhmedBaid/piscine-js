25f2623fdb11cd3b3623fe2e16b24fd2207cf8f7
abaid <you@example.com>
2025-07-06 13:45:02 +0100

    interpolation

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,3 +1,28 @@
-const retry = ()=>{
-    
+const retry = (count, callback) => {
+    let ctr = 0
+    let err
+    return async function (...params) {
+        while (ctr <= count) {
+            try {
+                return await callback(...params)
+            } catch (error) {
+                ctr++
+                err = error
+            }
+        }
+        if (ctr >= count) {
+            throw err;
+        }
+    }
+}
+
+const timeout = (delay, callback) => {
+    return (...params) => {
+        let reject = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject(new Error('timeout'))
+            },delay)
+        })  
+        return Promise.race([callback(...params),reject])
+    }
 }
