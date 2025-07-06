a2141054efe2e5d0b11178b82eed6b0761f6b43c
abaid <you@example.com>
2025-07-06 11:57:20 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -2,13 +2,12 @@ const interpolation = (obj = { step: 0, start: 0, end: callback, duration: 0 }) 
     let delay = obj.duration / obj.step
     let count = 1
     let x = (obj.end - obj.start) / obj.step
-    let y = obj.duration / obj.step
+    let y = 0
     let wahd = obj.start
-    let joj = 0
     let interval = setInterval(() => {
         if (count <= obj.step) {
-            joj += y
-            obj.callback([wahd, joj])
+            y += obj.duration / obj.step
+            obj.callback([wahd, y])
             wahd += x
             count++
         } else {
