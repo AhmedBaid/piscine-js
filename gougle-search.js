d2f2ef46b08ff4974983fee605fffb44c685b96d
abaid <you@example.com>
2025-07-07 17:58:17 +0100

    google-search

--- gougle-search.js
+++ gougle-search.js
@@ -1,4 +1,21 @@
-const queryServers = (serverName, q) => {
-    getJSON(`/${serverName}?${q}=hello+world`)
-    getJSON(`/${_backup}?${q}=hello+world`)
+const queryServers = (serverName, query) => {
+    let first = getJSON(`/${serverName}?q=${query}`)
+    let last = getJSON(`/${serverName}_backup?q=${query}`)
+    return Promise.race([first, last])
+}
+async function gougleSearch(query) {
+    const reject = new Promise((_, reject) =>
+        setTimeout(() => reject(new Error("timeout")), 80)
+    );
+
+    const result = Promise.all([
+        queryServers("web", query),
+        queryServers("image", query),
+        queryServers("video", query),
+    ]).then(([web, image, video]) => ({
+        web,
+        image,
+        video,
+    }))
+    return Promise.race([result, reject])
 }
