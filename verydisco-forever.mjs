585c2d3bf88bcb7688c2fb954c67deab36b1a2b8
abaid <you@example.com>
2025-07-09 16:05:40 +0100

    verydisco-forever.mjs

---
+++ verydisco-forever.mjs
@@ -0,0 +1,14 @@
+import { writeFile } from "fs/promises";
+const args = process.argv.slice(2);
+let arr = args[0].split(" ")
+
+let start
+let end
+let combine = ''
+arr.forEach(element => {
+    start = element.slice(Math.ceil(element.length / 2))
+    end = element.slice(0, Math.ceil(element.length / 2))
+    combine += start + end + ' '
+});
+let content = combine.trim()
+writeFile('./verydisco-forever.txt', content);
